e1b3791fd115a9dea4783c3d02f637eb8b8b173b
Jordan Firaben <72997882+jfiraben@users.noreply.github.com>
2021-01-25 16:54:05 -0500

    Create index.php

---
+++ assignments/assignment-2/assignment2/index.php
@@ -0,0 +1,87 @@
+<!--
+Jordan Firaben
+IT 207 - 002
+assignment2/index.php File
+-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<title>Lab Assignment 2</title>
+		<link rel="stylesheet" href="lab2.css" type="text/css"/>
+	</head>
+	<body>
+    <?php include '../header.php';?>
+    <?php include '../menu.inc';?>
+		<div id="content">
+			<div class="calendar">
+				<div class="month">
+	        March, 2018
+	      </div>
+	      <div class="row">
+	  			<div class="cell">Sunday</div>
+	  			<div class="cell">Monday</div>
+	  			<div class="cell">Tuesday</div>
+	  			<div class="cell">Wednesday</div>
+	  			<div class="cell">Thursday</div>
+	  			<div class="cell">Friday</div>
+	  			<div class="cell">Saturday</div>
+	  			<br class="clear"/>
+	  		</div>
+	  		<div class="row">
+	  			<div class="cell"></div>
+	  			<div class="cell"></div>
+	  			<div class="cell"></div>
+	  			<div class="cell"></div>
+	  			<div class="cell">1</div>
+	  			<div class="cell">2</div>
+	  			<div class="cell">3</div>
+	  			<br class="clear"/>
+	  		</div>
+	  		<div class="row">
+	  			<div class="cell">4</div>
+	  			<div class="cell">5</div>
+	  			<div class="cell">6</div>
+	  			<div class="cell">7</div>
+	  			<div class="cell">8</div>
+	  			<div class="cell">9</div>
+	  			<div class="cell">10</div>
+	  			<br class="clear"/>
+	  		</div>
+	  		<div class="row">
+	  			<div class="cell">11</div>
+	  			<div class="cell">12</div>
+	  			<div class="cell">13</div>
+	  			<div class="cell">14</div>
+	  			<div class="cell">15</div>
+	  			<div class="cell">16</div>
+	  			<div class="cell">17</div>
+	  			<br class="clear"/>
+	  		</div>
+				<div class="row">
+	  			<div class="cell">18</div>
+	  			<div class="cell">19</div>
+	  			<div class="cell">20</div>
+	  			<div class="cell">21</div>
+	  			<div class="cell">22</div>
+	  			<div class="cell">23</div>
+	  			<div class="cell">24</div>
+	  			<br class="clear"/>
+	  		</div>
+				<div class="row">
+	  			<div class="cell">25</div>
+	  			<div class="cell">26</div>
+	  			<div class="cell">27</div>
+	  			<div class="cell">28</div>
+	  			<div class="cell">29</div>
+	  			<div class="cell">30</div>
+	  			<div class="cell">31</div>
+	  			<br class="clear"/>
+	  		</div>
+			</div>
+		</div>
+    <?php include '../footer.inc';?>
+	</body>
+</html>
