a898d474842b12d648d929ff2e0a8135dd931209
Jordan Firaben <72997882+jfiraben@users.noreply.github.com>
2021-01-25 16:36:29 -0500

    Add files via upload

---
+++ in-class-exercies/week-3/phpexample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title>PHP Example</title>
+	</head>
+	<body>
+		<h2>PHP Example</h2>
+		<?php
+			print cal_days_in_month(CAL_GREGORIAN,2,2008);
+		?>
+		<?php
+			phpinfo();
+		?>
+	</body>
+</html>
